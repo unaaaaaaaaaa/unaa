c1286a8c63758d8ccfd5533de0353caf126cc8fb
unaaaaaaaaaa <100131900+unaaaaaaaaaa@users.noreply.github.com>
2022-02-21 20:33:51 +0800

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myAge = 35
+    printf(" I am =%d" ,myAge);
+    printf(" year old \n ")
+    cout<<"I am "<< myAge << " year old.";
+    return 0;
+}
